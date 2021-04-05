59aa06b61d7cef93c18912f42daca7a3a7695869
jmossm2 <55605490+jmossm2@users.noreply.github.com>
2021-04-05 17:47:15 -0500

    add web server and handlers
    4/5/2021 meeting

--- DogLaserToy.ino
+++ DogLaserToy.ino
@@ -1,20 +1,109 @@
 // #include "core-functions.h"
 // #include "backend.h"
-#include <ESP32Servo.h>
+// #include <ESP32Servo.h>
+// #include "esp_camera.h"
+#include <WiFi.h>
+#include "esp_http_server.h"
+#include "esp_camera.h"
 
-//#define LED_BUILTIN 2
+#define CAMERA_MODEL_AI_THINKER // Our camera model
+#define Wifi WiFi // Jaden can't spell WiFi
+
+// Some sent strings need this extra info, tedious to re-type
+#define PART_BOUNDARY "123456789000000000000987654321"
+static const char* _STREAM_CONTENT_TYPE = "multipart/x-mixed-replace;boundary=" PART_BOUNDARY;
+static const char* _STREAM_BOUNDARY = "\r\n--" PART_BOUNDARY "\r\n";
+static const char* _STREAM_PART = "Content-Type: image/jpeg\r\nContent-Length: %u\r\n\r\n";
+
+// #include "camera_pins.h"
+
+// WiFi login for my laptop's hotspot
+const char* ssid = "LAPTOP-1QLPB7T6 1976";
+const char* password = "1003P5<o";
+// Honestly IDK what this is
+httpd_handle_t web_httpd = NULL;
+
+// Handler for server
+static esp_err_t index_handler(httpd_req_t *req) {
+  httpd_resp_set_type(req, "text/plain");
+  return httpd_resp_send(req, "index handler part 1",20);
+}
+
+// Stream handler for server
+static esp_err_t stream_handler(httpd_req_t *req) {
+  camera_fb_t *fb = NULL;
+  esp_err_t res = ESP_OK;
+  // size_t _jpg_buf_len = 0;
+  // uint8_t *_jpg_buf = NULL;
+  // char *part_buf[64];
+  // dl_matrix3du_t *image_matrix = NULL;
+
+  // Cut out the weird if statement from the example, SHOULDNT cause issues
+  static int64_t last_frame = 0;
+  if (!last_frame) last_frame = esp_timer_get_time();
+
+  res = httpd_resp_set_type(req, _STREAM_CONTENT_TYPE);
+  if (res != ESP_OK) return res;
+
+  httpd_resp_set_hdr(req, "Access-Control-Allow-Origin", "*");
+
+  // Main streaming loop
+  while (true) {
+    fb = esp_camera_fb_get();
+    if (!fb) {
+      Serial.println("Camera capture failed");
+      res = ESP_FAIL;
+    } else {
+      // TODO: There is a buffer, do stuff
+    }
+    if (res == ESP_OK) {
+      res = httpd_resp_send_chunk(req, _STREAM_BOUNDARY, strlen(_STREAM_BOUNDARY));
+    }
+  }
+
+  last_frame = 0;
+  return res;
+  
+}
+
+void startServer() {
+  httpd_config_t config = HTTPD_DEFAULT_CONFIG();
+
+  httpd_uri_t index_uri = {
+    .uri      = "/",
+    .method   = HTTP_GET,
+    .handler  = index_handler,
+    .user_ctx = NULL
+  };
+
+  Serial.printf("Starting web server on port: '%d'\n", config.server_port);
+  if (httpd_start(&web_httpd, &config) == ESP_OK) {
+    httpd_register_uri_handler(web_httpd, &index_uri);
+  }
+}
+
 void setup() {
-  // put your setup code here, to run once:
   Serial.begin(115200);
-  //pinMode(LED_BUILTIN, OUTPUT);
+
+  // Connect to wifi
+  WiFi.begin(ssid, password);
+
+  // Wait until connected to continue
+  while (WiFi.status() != WL_CONNECTED) {
+    delay(500);
+    Serial.print(".");
+  }
+  Serial.println("\nWiFi connected");
+
+  // Start the web server
+  startServer();
+
+  // Tell user how to connect to server
+  Serial.print("Server ready! Use 'http://");
+  Serial.print(WiFi.localIP());
+  Serial.println("' to connect");
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
-  //digitalWrite(LED_BUILTIN, HIGH);
-  Serial.printf("HIGH\n");
-  delay(500);
-  //digitalWrite(LED_BUILTIN, HIGH);
-  Serial.printf("LOW\n");
-  delay(500);
+  delay(10000);
 }
